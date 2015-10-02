2b0ccd66cff8d7ae5346442f584290ffddfc97bb
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-10-02 11:31:34 -0300

    right user

--- scripts/cronjob.sh
+++ scripts/cronjob.sh
@@ -4,4 +4,4 @@ source /home/ubuntu/.profile;
 spark-submit --master spark://hdfs-namenode:7077 /home/ubuntu/gabdi/wikitrends/scripts/wiki_trends.py hdfs://hdfs-namenode:9000/data/dataset/data.json http://hdfs-namenode:50070 ubuntu >> /var/log/gabdi/gabdi.log 2>&1
 rm -rf /tmp/*tsv
 hdfs dfs -get /data/serving/* /tmp/
-mv /tmp/*tsv /var/www/wikitrends/data/
+sudo mv /tmp/*tsv /var/www/wikitrends/data/
