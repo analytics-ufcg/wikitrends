e1cfef91e5300a14bc1c433b1a9600355e8dbc55
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-10-13 15:15:42 -0300

    merge OK (apparently)

---
+++ scripts/job_speed.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/ubuntu/.profile;
+spark-submit --master spark://hdfs-namenode:7077 --total-executor-cores 1 /home/ubuntu/gabdi/wikitrends/scripts/streaming_client.py hdfs://hdfs-namenode:9000/user/ubuntu >> /var/log/gabdi/gabdi-streaming.log 2>&1
