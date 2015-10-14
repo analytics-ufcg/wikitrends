09322200b731f0c0c1a805b747c9914b0f03576c
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-10-14 17:08:19 -0300

    fixing getmerge

--- scripts/cronjob_merge.sh
+++ scripts/cronjob_merge.sh
@@ -7,6 +7,6 @@ done
 echo "done"
 touch /tmp/.gabdi.lock
 last=`hdfs dfs -ls /user/ubuntu/speed_tmp/ | tail -1 | awk '{print $8}'`
-hdfs dfs -getmerge ${last} > /tmp/absolute-realtime.tsv
+hdfs dfs -getmerge ${last} /tmp/absolute-realtime.tsv
 sudo bash /home/ubuntu/gabdi/wikitrends/scripts/merge_layers.sh /tmp/ /var/www/wikitrends/data/
 rm /tmp/.gabdi.lock
