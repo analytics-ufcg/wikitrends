f3f773545ef1f62499348799853fbf3d73db318b
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-10-14 17:00:42 -0300

    merging layers

--- scripts/cronjob_merge.sh
+++ scripts/cronjob_merge.sh
@@ -6,7 +6,7 @@ sleep 1
 done
 echo "done"
 touch /tmp/.gabdi.lock
-#hdfs dfs -getmerge /user/ubuntu/serving/absolute-realtime/part-00000 > /tmp/absolute-realtime.tsv
-touch /tmp/absolute-realtime.tsv
+last=`hdfs dfs -ls /user/ubuntu/speed_tmp/ | tail -1 | awk '{print $8}'`
+hdfs dfs -getmerge ${last} > /tmp/absolute-realtime.tsv
 sudo bash /home/ubuntu/gabdi/wikitrends/scripts/merge_layers.sh /tmp/ /var/www/wikitrends/data/
 rm /tmp/.gabdi.lock
