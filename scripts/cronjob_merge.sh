80a9f6ef5907e7aa5bac698d69fffaf77074b275
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-10-14 23:18:00 -0300

    solving phantom blue numbers bug

--- scripts/cronjob_merge.sh
+++ scripts/cronjob_merge.sh
@@ -6,7 +6,7 @@ sleep 1
 done
 echo "done"
 touch /tmp/.gabdi.lock
-last=`hdfs dfs -ls /user/ubuntu/speed_tmp/ | tail -1 | awk '{print $8}'`
-hdfs dfs -getmerge ${last} /tmp/absolute-realtime.tsv
+last=`hdfs dfs -ls /user/ubuntu/speed_tmp/*/_SUCCESS | tail -1 | awk '{print $8}'`
+hdfs dfs -getmerge ${last%/*} /tmp/absolute-realtime.tsv
 sudo bash /home/ubuntu/gabdi/wikitrends/scripts/merge_layers.sh /tmp/ /var/www/wikitrends/data/
 rm /tmp/.gabdi.lock
