c53c35a002fbddef64d9eff2418c7cabadfc72cd
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-11-05 09:38:01 -0300

    simplifying spark streaming job

--- cron/python/job_speed.sh
+++ cron/python/job_speed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/ubuntu/.profile;
-spark-submit --master spark://hdfs-namenode:7077 --total-executor-cores 2 --py-files ../../spark/python/wiki_trends.py,../../spark/python/config.py spark/python/streaming_client.py hdfs://hdfs-namenode:9000/user/ubuntu >> /var/log/gabdi/gabdi-streaming.log 2>&1
+spark-submit --master spark://hdfs-namenode:7077 --total-executor-cores 2 --py-files ../../spark/python/wiki_trends.py,../../spark/python/config.py ../../spark/python/streaming_client.py hdfs://hdfs-namenode:9000/user/ubuntu >> /var/log/gabdi/gabdi-streaming.log 2>&1
--- spark/python/streaming_client.py
+++ spark/python/streaming_client.py
@@ -59,16 +59,19 @@ if __name__ == "__main__":
 
     lines_dstream = scc.socketTextStream(config.STREAMING_CLIENT_CONFIG['HOST'],
                                          config.STREAMING_CLIENT_CONFIG['PORT'])
+    proc_type = 'speed_tmp'
 
-    lines_dstream.count().pprint()
+    lines_dstream.map(lambda edit: ("stream_all_edits", 1)).reduceByKey(lambda a, b: a+b).saveAsTextFiles("{0}/{1}/{2}".format(hdfs_user_folder, proc_type, 'absolute'), "tsv")
 
-    parsed_edits, failed_edits = parse_edits_rt(lines_dstream)
-    parsed_edits.count().pprint()
+    # lines_dstream.count().pprint()
 
-    parsed_edits = wiki_trends.clean_rdd(parsed_edits)
-    parsed_edits.count().pprint()
+    # parsed_edits, failed_edits = parse_edits_rt(lines_dstream)
+    # parsed_edits.count().pprint()
 
-    process_absolute_data_rt(parsed_edits, hdfs_user_folder)
+    # parsed_edits = wiki_trends.clean_rdd(parsed_edits)
+    # parsed_edits.count().pprint()
+
+    # process_absolute_data_rt(parsed_edits, hdfs_user_folder)
 
     scc.start()
     scc.awaitTermination()
