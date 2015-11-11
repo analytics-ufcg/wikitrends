15d38e5433f58e2789e50182121701b2fd49499c
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-11-11 14:15:56 -0300

    batch job in java

---
+++ spark/java/Wikitrends/src/main/java/br/edu/ufcg/analytics/wikitrends/spark/BatchLayerOutput.java
@@ -0,0 +1,39 @@
+package br.edu.ufcg.analytics.wikitrends.spark;
+
+import java.io.Serializable;
+
+/**
+ * Output formatter
+ * 
+ * @author Ricardo Ara&eacute;jo Santos - ricoaraujosantos@gmail.com
+ */
+public class BatchLayerOutput implements Serializable{
+
+	private static final long serialVersionUID = 1595860019246486013L;
+	
+	private String key;
+	private String value;
+
+	/**
+	 * Default constructor
+	 * @param key
+	 * @param value
+	 */
+	public BatchLayerOutput(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+	public BatchLayerOutput(String key, Long countAllEdits) {
+		this(key, countAllEdits.toString());
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return key + '\t' + value;
+	}
+
+}
