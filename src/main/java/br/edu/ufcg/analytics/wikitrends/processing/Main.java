355b997ed7b318a27bf4ce60ff054cbe8e5c11a6
Felipe Vieira <felipe29vieira@gmail.com>
2015-11-18 17:44:36 -0300

    adding ingestor option to wiktrends script

--- src/main/java/br/edu/ufcg/analytics/wikitrends/processing/Main.java
+++ src/main/java/br/edu/ufcg/analytics/wikitrends/processing/Main.java
@@ -1,8 +1,12 @@
 package br.edu.ufcg.analytics.wikitrends.processing;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.lang3.EnumUtils;
 
 /**
  * Entry point for both batch and speed layer Apache Spark jobs.
@@ -16,8 +20,11 @@ public class Main {
 	/**
 	 * Usage: spark-submit --class br.edu.ufcg.analytics.wikitrends.Main JAR_FILE_NAME.jar &lt;seed&gt;
 	 * @throws ConfigurationException when a proper configuration file cannot be found. 
+	 * @throws ExecutionException 
+	 * @throws InterruptedException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws ConfigurationException {
+	public static void main(String[] args) throws ConfigurationException, IOException, InterruptedException, ExecutionException {
 		
 		if (args.length < 1 || args.length > 2) {
 			System.err.println("Usage: java -cp <path-to-jar-files> Main <LAMBDA LAYER> [wikitrends.properties]");
@@ -26,7 +33,14 @@ public class Main {
 		
 		Configuration configuration = new PropertiesConfiguration(args.length == 2? args[1]: DEFAULT_CONFIG_FILEPATH);
 		
-		LambdaLayer.valueOf(args[0]).buildJob(configuration).run();
+		if (EnumUtils.isValidEnum(LambdaLayer.class, args[0])) {
+			LambdaLayer.valueOf(args[0]).buildJob(configuration).run();
+		}
+		
+		else {
+			ExternalServicesEnum.valueOf(args[0]).startService(configuration);
+		}
+		
 	}
 }
 
