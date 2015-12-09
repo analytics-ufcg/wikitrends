9ef7ba275dc8f99a910556019362af97a6aebec8
Ricardo Araújo <ricoaraujosantos@gmail.com>
2015-12-09 16:55:43 -0300

    updating raw types

--- src/test/java/br/edu/ufcg/analytics/wikitrends/integration/SmallDataBatch1IT.java
+++ src/test/java/br/edu/ufcg/analytics/wikitrends/integration/SmallDataBatch1IT.java
@@ -11,7 +11,9 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.datastax.driver.core.Cluster;
@@ -39,40 +41,61 @@ public class SmallDataBatch1IT {
 	private PropertiesConfiguration configuration;
 	private Cluster cluster;
 	private Session session;
-	private CassandraMasterDatasetManager master_dataset_manager;
-	private CassandraServingLayer1Manager serving_layer_manager;
-	
 
+
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void prepareMasterDataset() throws Exception {
+		System.setProperty("spark.cassandra.connection.host", SEED_NODE);
+		System.setProperty("spark.master", "local");
+		System.setProperty("spark.app.name", "small-test");
+
+		cleanMasterDataset();
+
+		try(
+				Cluster cluster = Cluster.builder().addContactPoints(SEED_NODE).build();
+				Session session = cluster.newSession();
+				){
+
+			new CassandraMasterDatasetManager().createTables(session);
+			new CassandraServingLayer1Manager().createTables(session);
+
+		}
+
+		new CassandraMasterDatasetManager().populate(INPUT_FILE);
+	}
 	
+	/**
+	 * Clean master dataset
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void cleanMasterDataset() throws Exception {
+
+		try (Cluster cluster = Cluster.builder().addContactPoints(SEED_NODE).build();
+				Session session = cluster.newSession();) {
+			new CassandraMasterDatasetManager().dropTables(session);
+			new CassandraServingLayer1Manager().dropTables(session);
+		}
+
+	}
+
+
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		
-		System.setProperty("spark.cassandra.connection.host", SEED_NODE);
-		System.setProperty("spark.master", "local");
-		System.setProperty("spark.app.name", "small-test");
 
-		
-		
+		configuration = new PropertiesConfiguration(TEST_CONFIGURATION_FILE);
+
 		cluster = Cluster.builder().addContactPoints(SEED_NODE).build();
 		session = cluster.newSession();
-		
-		
-		configuration = new PropertiesConfiguration(TEST_CONFIGURATION_FILE);
-		master_dataset_manager = new CassandraMasterDatasetManager();
-		serving_layer_manager = new CassandraServingLayer1Manager();
-		
-		master_dataset_manager.dropTables(session);
-		serving_layer_manager.dropTables(session);
-		
-		master_dataset_manager.createTables(session);
-		serving_layer_manager.createTables(session);
-
 		session.execute("USE batch_views");
-		
-		master_dataset_manager.populate(INPUT_FILE);
+
 	}
 
 	/**
@@ -80,89 +103,85 @@ public class SmallDataBatch1IT {
 	 */
 	@After
 	public void tearDown() throws Exception {
-//		master_dataset_manager.dropTables(session);
-//		serving_layer_manager.dropTables(session);
-		
 		session.close();
 		cluster.close();
 	}
+
 
 	/**
 	 * @throws ConfigurationException
 	 */
 	@Test
 	public void testProcessTopEditors() throws ConfigurationException {
-		
-		TopEditorsBatch1 job1 = new TopEditorsBatch1(configuration);
-		job1.setCurrentTime(LocalDateTime.of(2015, 11, 9, 14, 00));//FIXME wrong date
-		job1.process();
-	
-		try (Cluster cluster = Cluster.builder().addContactPoints(SEED_NODE).build();
-				Session session = cluster.newSession();) {
-			
-			assertEquals(327, session.execute("SELECT count(1) FROM batch_views.top_editors").one().getLong("count"));
-			assertEquals(510, session.execute("SELECT sum(count) as ranking_sum FROM batch_views.top_editors").one().getLong("ranking_sum"));
 
-			long rankingMax = session.execute("SELECT max(count) as ranking_max FROM batch_views.top_editors").one().getLong("ranking_max");
-			long rankingFirst = session.execute("SELECT count as ranking_max FROM batch_views.top_editors LIMIT 1").one().getLong("ranking_max");
-			
-			assertEquals(31, rankingMax);
-			assertEquals(31, rankingFirst);
-		}
+		TopEditorsBatch1 job = new TopEditorsBatch1(configuration);
+		job.setCurrentTime(LocalDateTime.of(2015, 11, 9, 14, 00));
+		job.process();
+
+		assertEquals(327, session.execute("SELECT count(1) FROM batch_views.top_editors").one().getLong("count"));
+		assertEquals(510, session.execute("SELECT sum(count) as ranking_sum FROM batch_views.top_editors").one().getLong("ranking_sum"));
+
+		long rankingMax = session.execute("SELECT max(count) as ranking_max FROM batch_views.top_editors").one().getLong("ranking_max");
+		long rankingFirst = session.execute("SELECT count as ranking_max FROM batch_views.top_editors LIMIT 1").one().getLong("ranking_max");
+
+		assertEquals(31, rankingMax);
+		assertEquals(31, rankingFirst);
 	}
-	
-	
+
+
 	/**
 	 * @throws ConfigurationException
 	 */
 	@Test
 	public void testProcessTopPages() throws ConfigurationException {
-		TopPagesBatch1 job2 = new TopPagesBatch1(configuration);
-		job2.process();
-		
+		TopPagesBatch1 job = new TopPagesBatch1(configuration);
+		job.setCurrentTime(LocalDateTime.of(2015, 11, 9, 14, 00));
+		job.process();
+
 		ResultSet resultSet = session.execute("SELECT count(1) FROM top_pages");
 		assertEquals(490, resultSet.one().getLong("count"));
-		
+
 		resultSet = session.execute("SELECT count(*) FROM top_pages WHERE count >= 3 ALLOW FILTERING");
 		assertEquals(3, resultSet.one().getLong("count"));
-		
+
 		resultSet = session.execute("SELECT count(*) FROM top_pages WHERE count = 2 ALLOW FILTERING");
 		assertEquals(14, resultSet.one().getLong("count"));
-		
+
 		resultSet = session.execute("SELECT * FROM top_pages WHERE count = 3 AND name = 'Marie Antoinette' ALLOW FILTERING");
 		List<Row> list = resultSet.all();
 		assertTrue(list.size() == 1);
 		assertTrue(list.get(0).getLong("count") == 3L);
-		
+
 		resultSet = session.execute("SELECT * FROM top_pages WHERE count = 3 AND name = 'Simone Zaza' ALLOW FILTERING");
 		List<Row> list2 = resultSet.all();
 		assertTrue(list2.size() == 1);
 		assertTrue(list2.get(0).getString("name").equals("Simone Zaza"));
 	}
-	
-	
+
+
 	/**
 	 * @throws ConfigurationException
 	 */
 	@Test
 	public void testProcessTopIdioms() throws ConfigurationException {
-		TopIdiomsBatch1 job3 = new TopIdiomsBatch1(configuration);
-		job3.process();
-		
+		TopIdiomsBatch1 job = new TopIdiomsBatch1(configuration);
+		job.setCurrentTime(LocalDateTime.of(2015, 11, 9, 14, 00));
+		job.process();
+
 		ResultSet resultSet = session.execute("SELECT count(1) FROM top_idioms");
 		assertEquals(45, resultSet.one().getLong("count"));
-		
+
 		resultSet = session.execute("SELECT count(*) FROM top_idioms WHERE count >= 3 ALLOW FILTERING");
 		assertEquals(19, resultSet.one().getLong("count"));
-		
+
 		resultSet = session.execute("SELECT count(*) FROM top_idioms WHERE count = 2 ALLOW FILTERING");
 		assertEquals(12, resultSet.one().getLong("count"));
-		
+
 		resultSet = session.execute("SELECT * FROM top_idioms WHERE count = 115 AND name = 'en.wikipedia.org' ALLOW FILTERING");
 		List<Row> list = resultSet.all();
 		assertTrue(list.size() == 1);
 		assertTrue(list.get(0).getLong("count") == 115L);
-		
+
 		resultSet = session.execute("SELECT * FROM top_idioms WHERE count = 55 AND name = 'it.wikipedia.org' ALLOW FILTERING");
 		List<Row> list2 = resultSet.all();
 		assertTrue(list2.size() == 1);
