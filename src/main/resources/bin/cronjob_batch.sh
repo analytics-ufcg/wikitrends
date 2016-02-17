b85d8b6a1620e4a1ff4de2120b1c886c579ab103
Ricardo Araújo <ricoaraujosantos@gmail.com>
2016-02-17 14:56:09 -0300

    adding cassandra nodes address

--- src/main/resources/bin/cronjob_batch.sh
+++ src/main/resources/bin/cronjob_batch.sh
@@ -2,10 +2,10 @@
 source /home/ubuntu/.profile;
 echo " ******************* starting batch layer **********************" >> /var/log/gabdi/gabdi.log
 date >> /var/log/gabdi/gabdi.log 2>&1
-./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch idioms >> /var/log/gabdi/gabdi.log 2>&1
-./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch editors >> /var/log/gabdi/gabdi.log 2>&1
-./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch content_pages >> /var/log/gabdi/gabdi.log 2>&1
-./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch metrics >> /var/log/gabdi/gabdi.log 2>&1
-./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch pages >> /var/log/gabdi/gabdi.log 2>&1
+./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 -c worker-1,worker-2 batch idioms >> /var/log/gabdi/gabdi.log 2>&1
+./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 -c worker-3,worker-4 batch editors >> /var/log/gabdi/gabdi.log 2>&1
+./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 -c worker-1,worker-2 batch content_pages >> /var/log/gabdi/gabdi.log 2>&1
+./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 -c worker-3,worker-4 batch metrics >> /var/log/gabdi/gabdi.log 2>&1
+./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 -c worker-1,worker-2 batch pages >> /var/log/gabdi/gabdi.log 2>&1
 date >> /var/log/gabdi/gabdi.log 2>&1
 echo " ******************* finished batch layer **********************" >> /var/log/gabdi/gabdi.log
