b7bd37ee4c687d34915529cbf9d1e3010917fc4e
Ricardo Araújo <ricoaraujosantos@gmail.com>
2016-02-17 14:04:07 -0300

    adding one more worker and changing execution order

--- src/main/resources/bin/cronjob_batch.sh
+++ src/main/resources/bin/cronjob_batch.sh
@@ -3,9 +3,9 @@ source /home/ubuntu/.profile;
 echo " ******************* starting batch layer **********************" >> /var/log/gabdi/gabdi.log
 date >> /var/log/gabdi/gabdi.log 2>&1
 ./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch idioms >> /var/log/gabdi/gabdi.log 2>&1
-./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch pages >> /var/log/gabdi/gabdi.log 2>&1
 ./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch editors >> /var/log/gabdi/gabdi.log 2>&1
 ./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch content_pages >> /var/log/gabdi/gabdi.log 2>&1
 ./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch metrics >> /var/log/gabdi/gabdi.log 2>&1
+./wikitrends -e /home/ubuntu/.profile -m spark://master:7077 -w 3 batch pages >> /var/log/gabdi/gabdi.log 2>&1
 date >> /var/log/gabdi/gabdi.log 2>&1
 echo " ******************* finished batch layer **********************" >> /var/log/gabdi/gabdi.log
